5237dc9648703e719733f1cd8a30ba4d80ecf210
R3qUi3M86 <requiem123@gmail.com>
2022-01-30 09:35:55 +0100

    implemented board column placeholders

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -13,6 +13,12 @@ export const buttonTypes = {
     boardTitleBtnGroup: "Board Title Group"
 }
 
+export const columnTypes = {
+    templateColumn: "Column template",
+    loadedColumn: "Column with data",
+    placeholderColumn: "Column with placeholders"
+}
+
 export function htmlFactory(template) {
     switch (template) {
         case htmlTemplates.button:
@@ -32,6 +38,7 @@ export function htmlFactory(template) {
 function boardBuilder(boardData) {
     const boardContainer = document.createElement('div');
     const boardContent = document.createElement('div');
+    const boardColumnContainer = document.createElement('div');
 
     const titleBtnGroup = buttonBuilder(buttonTypes.boardTitleBtnGroup,
                                         'secondary',
@@ -39,9 +46,18 @@ function boardBuilder(boardData) {
                                         boardData.id,
                                         boardData.title)
 
+    boardColumnContainer.classList.add("d-flex", "overflow-auto", 'board-column-container')
+
     boardContent.id = `collapse${boardData.id}`;
     boardContent.classList.add("collapse", "board-collapse");
     boardContent.setAttribute('data-board-id', boardData.id);
+    boardContent.appendChild(boardColumnContainer);
+
+    for (const i in boardData['statuses']){
+        const statusColumnId = boardData['statuses'][i]
+        boardColumnContainer.appendChild(columnBuilder(columnTypes.placeholderColumn, statusColumnId, boardData.id));
+    }
+
 
     boardContainer.id = `board-container-${boardData.id}`
     boardContainer.classList.add('bg-secondary', 'border-secondary', 'board-container', 'my-5')
@@ -172,6 +188,7 @@ function rowFormBuilder(defaultText, btnStyle, parent) {
     input.classList.add('form-control');
     input.setAttribute('type', 'text');
     input.setAttribute('placeholder', defaultText);
+    input.setAttribute('value', defaultText);
 
     form.id = 'changeTitleForm';
     form.classList.add('newBoardTitleForm');
@@ -186,20 +203,66 @@ function rowFormBuilder(defaultText, btnStyle, parent) {
     return form;
 }
 
-function boardColumnPlaceholderBuilder(){
-    const column = document.createElement('div');
-    const columnBody = document.createElement('div');
-    const columnTitle = document.createElement('h5');
-    const columnCardContainer = document.createElement('div')
+function columnBuilder(type, statusColumnId, boardId, name) {
+    switch (type) {
+        case columnTypes.templateColumn:
+            const column = document.createElement('div');
+            const columnBody = document.createElement('div');
+            const tittleGroup = document.createElement('div')
 
-    columnTitle.classList.add('card-title', 'placeholder-glow');
+            const settingsButton = buttonBuilder(buttonTypes.settingsBtn,
+                                    'btn-antracite',
+                                    'btn-size-small',
+                                    `board-${boardId}-column-${statusColumnId}-title`)
 
-    columnBody.classList.add('card-body');
-    columnBody.appendChild(columnTitle)
-    columnBody.appendChild(columnCardContainer)
+            settingsButton.children[0].classList.add('disabled')
 
-    column.classList.add('card', 'text-dark', 'bg-light', 'mx-3');
-    column.setAttribute('aria-hidden', 'true');
+            tittleGroup.classList.add("btn-group", 'd-flex');
+            tittleGroup.setAttribute('data-board-id', boardId);
+            tittleGroup.setAttribute('data-status-column-id', statusColumnId)
+            tittleGroup.setAttribute('role', "group")
+            tittleGroup.appendChild(settingsButton);
+
+            columnBody.id = `status-column-${statusColumnId}-body`
+            columnBody.classList.add('card-body', 'column-body', 'px-2', 'py-2', "overflow-auto");
+            columnBody.setAttribute('data-board-id', boardId);
+            columnBody.setAttribute('data-status-column-id', statusColumnId)
+            columnBody.appendChild(tittleGroup);
+
+            column.id = `status-column-${statusColumnId}`
+            column.classList.add('card', 'text-light', 'bg-dark', 'bg-antracite', 'mx-1', 'my-1', 'status-column');
+            column.setAttribute('aria-hidden', 'false');
+            column.setAttribute('data-board-id', boardId);
+            column.setAttribute('data-status-column-id', statusColumnId)
+            column.appendChild(columnBody)
+
+            return column;
+
+        case columnTypes.placeholderColumn:
+            const templateColumn = columnBuilder(columnTypes.templateColumn, statusColumnId, boardId)
+            const titleWrapper = document.createElement('div')
+            const titlePlaceholder1 = document.createElement('span')
+            const titlePlaceholder2 = document.createElement('span')
+
+            titlePlaceholder1.classList.add('placeholder', 'col-6');
+            titlePlaceholder2.classList.add('placeholder', 'col-3');
+
+            titleWrapper.id = `board-${boardId}-column-${statusColumnId}-title`
+            titleWrapper.classList.add('card-text', 'column-title', 'placeholder-wave', 'h5', 'flex-grow-1', 'hstack', 'mb-0');
+            titleWrapper.setAttribute('data-board-id', boardId);
+            titleWrapper.setAttribute('data-status-column-id', statusColumnId)
+            titleWrapper.appendChild(titlePlaceholder1)
+            titleWrapper.append("\u00A0");
+            titleWrapper.appendChild(titlePlaceholder2)
+
+            templateColumn.children[0].children[0].insertBefore(titleWrapper, templateColumn.children[0].children[0].children[0])
+
+            return templateColumn;
+    }
+}
+
+function boardColumnPlaceholderBuilder(statusColumnId){
+
 }
 
 function boardCardPlaceholderBuilder(){
