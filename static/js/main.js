70de7260421773fc931658d06af506356b972dcf
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-02-09 21:29:11 +0100

    manual synchronization

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 import { boardsManager } from "./controller/boardsManager.js";
 import { buttonsManager } from "./controller/buttonsManager.js";
 
-const timerInterval = setInterval(boardsManager.loadBoardContent, 1000);
+// const timerInterval = setInterval(boardsManager.loadBoardContent, 1000);
 
 function init() {
   buttonsManager.createAddBoardButton();
